733c2d63dcecedd7bd3fce864140c7a07894fd25
ym973010 <ym970310@gmail.com>
2025-01-20 17:28:27 +0900

    로그인 관련 추가(Member,security)

--- Back/mesbiens/src/main/java/mesbiens/member/repository/MemberRepository.java
+++ Back/mesbiens/src/main/java/mesbiens/member/repository/MemberRepository.java
@@ -1,12 +1,21 @@
 package mesbiens.member.repository;
 
 import java.util.Optional;
-import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import mesbiens.member.vo.MemberVO;
 
-public interface MemberRepository extends CrudRepository<MemberVO, Integer> {
+public interface MemberRepository extends JpaRepository<MemberVO, Integer> {
 
     // 회원 번호(memberNo)를 기준으로 조회하는 메소드
     Optional<MemberVO> findByMemberNo(int memberNo);
+    
+    // 이메일을 기준으로 조회하는 메소드
+    Optional<MemberVO> findByMemberEmail(String memberEmail);
+    
+
+    // ID를 기준으로 조회하는 메소드
+    Optional<MemberVO> findByMemberId(String memberId);
     
 }
